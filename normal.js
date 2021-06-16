5444cb7de2792d025d3c40ecb5a60b0249999a04
Srinjay-JavaScript <71552411+Srinjay-JavaScript@users.noreply.github.com>
2021-06-16 16:44:15 +0400

    Add files via upload

--- normal.js
+++ normal.js
@@ -1,37 +1,44 @@
-wristX = 0;
 wristY = 0;
+wristX = 0;
+started = false;
+
 ScoreRightWrist = 0;
 function preload(){
 }
 
 function setup() {
-// canvas = createCanvas(700, 450);
-	video = createCapture(VIDEO);
-	video.size(700, 400);
-	video.parent('webcam');
+  canvas = createCanvas(640, 450);
+	canvas.parent('webcam');
+  video = createCapture(VIDEO);
+  video.hide();
 	posenet = ml5.poseNet(video, init);
 }
 function draw(){
-  // image(video, 0, 0, 700, 400);
-  // if (ScoreRightWrist > 0.2){
-  //   fill('red');
-  //   stroke('red');
-  //   circle(wristX, wristY, 20);
-  // }
+  image(video, 0, 0, 640, 450)
+  if (started == true){
+  if (ScoreRightWrist > 0.2){
+    fill('red');
+    stroke('red');
+    circle(wristX, wristY-20, 20);
+  }
 }
-// function play(){
-//     posenet.on('pose', got_result);
-// }
-// function init(){
-//     console.log('Posenet has successfully initialized.');
-//   }
+}
+function play(){
+    started = true;
+    document.getElementById('status').innerHTML = 'Status: Game has successfully launched.';
 
-  function got_result(results){
+}
+function init(){
+    console.log('Posenet has successfully initialized.');
+  }
+
+  function get_result(results){
     if (results.length > 0){
-      wristX = results[0].pose.rightWrist.x;
       wristY = results[0].pose.rightWrist.y;
-      console.log('WristX: ' + wristX + ' WristY: ' + wristY);
+      wristX = results[0].pose.rightWrist.x;
+
+      console.log('WristY: ' + wristY + ' WristX: ' + wristX);
       ScoreRightWrist = results[0].pose.keypoints[10].score;
      
     }
- }
+  }
